c0537d35da5076c2cba0d3e60d8d70424bd4ef5b
moonjiyoung <umarg228@naver.com>
2024-05-05 20:51:59 +0900

    feat:mongodb연결 오류 해결

--- util/database.tsx
+++ util/database.tsx
@@ -1,15 +1,18 @@
-import { MongoClient } from "mongodb";
+import { MongoClient, MongoClientOptions } from "mongodb";
 const url =
   "mongodb+srv://mongzii228:qwer1234@cluster0.m8aww3r.mongodb.net/?retryWrites=true&w=majority";
-const options: any = { useNewUrlParser: true };
+//const options: MongoClientOptions = { useNewUrlParser: true };
+//const options: MongoClientOptions = { useUnifiedTopology: true };
 let connectDB: Promise<MongoClient>;
 
 if (process.env.NODE_ENV === "development") {
   if (!global._mongo) {
-    global._mongo = new MongoClient(url, options).connect();
+    // global._mongo = new MongoClient(url, options).connect();
+    global._mongo = new MongoClient(url).connect();
   }
   connectDB = global._mongo;
 } else {
-  connectDB = new MongoClient(url, options).connect();
+  //connectDB = new MongoClient(url, options).connect();
+  connectDB = new MongoClient(url).connect();
 }
 export { connectDB };
