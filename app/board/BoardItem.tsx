4113e2061a0b30abc9687b089e87472f95f329da
moonjiyoung <umarg228@naver.com>
2024-05-12 18:20:02 +0900

    feat: Q&A페이지하다가 오류싸움 이기지못함

---
+++ app/board/BoardItem.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { ObjectId } from "mongodb";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+interface IboardItem {
+  _id: string;
+  title: string;
+  content: string;
+  author?: string;
+}
+interface BoardProps {
+  data: Promise<IboardItem[]>;
+}
+
+export default function BoardItem({ data }: BoardProps) {
+  //  const [post, setPost] = useState<IboardItem[]>([]);
+
+  //   useEffect(() => {
+  //     data.then(res => setPost(res)).catch(error => console.log(error));
+  //   }, [data]);
+
+  //   console.log(post);
+  // console.log(data);
+  const promiseTest = (data: any) =>
+    new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (data) {
+          resolve("ok?");
+          console.log(data);
+        } else {
+          reject("nn?");
+        }
+      }, 1000);
+    })
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => console.log(err));
+  return (
+    <div>
+      <h2>Q & A</h2>
+      <button onClick={data => promiseTest(data)}></button>
+    </div>
+  );
+}
+
+// export default function BoardItem({ data }: BoardProps) {
+//   //   useEffect(() => {
+//   //     data.then(res => {
+//   //       console.log(res);
+//   //     });
+//   //   }, [data]);
+
+//   return (
+//     <div>
+//       <h2>Q & A</h2>
+//     </div>
+//   );
+// }
+
+// export default function BoardItem() {
+//   return (
+//     <div>
+//       <h2>Q & A</h2>
+//     </div>
+//   );
+// }
--- app/board/page.tsx
+++ app/board/page.tsx
@@ -1,8 +1,26 @@
 import { connectDB } from "@/util/database";
+import BoardItem from "./BoardItem";
+import { useState, useEffect } from "react";
 
 export default async function Qa() {
   const client = await connectDB;
   const db = client.db("market");
   const data = await db.collection("post").find().toArray();
+
   //console.log(data);
+  // const data = rawData.map((item: any) => ({
+  //   _id: item._id.toString(),
+  //   title: item.title,
+  //   content: item.content,
+  //   author: item.author,
+  // }));
+  // console.log(data);
+
+  return (
+    <div className="list-bg">
+      <BoardItem data={data} />
+      {/* <BoardItem /> */}
+      {/* <DetailLink /> */}
+    </div>
+  );
 }
