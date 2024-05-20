e8889caf848c3e9f8131ffb3a2e4bf0fdd30b11d
moonjiyoung <umarg228@naver.com>
2024-05-20 16:59:26 +0900

    feat: member페이지정리

--- app/member/page.tsx
+++ app/member/page.tsx
@@ -2,9 +2,9 @@
 
 export default function Member() {
   return (
-    <div>
-      <h2>회원가입</h2>
-      <form method="POST" action="/api/auth/signup">
+    <div className="member-body">
+      <h2 className="member-title">회원가입</h2>
+      <form method="POST" action="/api/auth/signup" className="member-main">
         <input name="name" type="text" placeholder="이름" />
         <input name="email" type="text" placeholder="email" />
         <input name="password" type="password" placeholder="비밀번호" />
