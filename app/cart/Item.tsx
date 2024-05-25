4935fd7921787c4b635d3229573e959f46b431aa
moonjiyoung <umarg228@naver.com>
2024-05-25 16:16:20 +0900

    feat: 로그인안되었을시 카트페이지차단

--- app/cart/Item.tsx
+++ app/cart/Item.tsx
@@ -6,12 +6,14 @@ import { deletecart, countUp, countDown } from "../../redux/cartSlice";
 import { useRouter } from "next/navigation";
 import { useState, useEffect } from "react";
 import type { Icart } from "../../redux/cartSlice";
+import { useSession } from "next-auth/react";
 
 interface Irawdata {
   cartState: Icart[];
 }
 
 export default function Item() {
+  const { data: session, status } = useSession();
   let router = useRouter();
   const Item: Icart[] = useSelector((state: RootState) => state.cartState);
   const dispatch = useDispatch();
@@ -33,7 +35,40 @@ export default function Item() {
 
   return (
     <div>
-      <h2 className="title-cart">장바구니</h2>
+      {status === "unauthenticated" ? (
+        <h1 className="login-cart">로그인 먼저하세요</h1>
+      ) : (
+        <div>
+          <h2 className="title-cart">cart페이지</h2>
+          {Item.map((el, i) => {
+            return (
+              <div className="cart-item" key={i}>
+                <div className="item1">
+                  <p>{el.product}</p>
+                </div>
+                <div className="item2">
+                  <p>{el.price}원</p>
+                </div>
+                <div className="item3">
+                  <button onClick={() => handleUp(i)}>+</button>
+                  <p>{el.count}</p>
+                  <button onClick={() => handleDown(i)}>-</button>
+                </div>
+                <div className="item4">
+                  <button
+                    onClick={() =>
+                      handleDelete(el.id, el.product, el.price, el.count)
+                    }
+                  >
+                    삭제
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      )}
+      {/* <h2 className="title-cart">장바구니</h2>
       {Item.map((el, i) => {
         return (
           <div className="cart-item" key={i}>
@@ -59,7 +94,7 @@ export default function Item() {
             </div>
           </div>
         );
-      })}
+      })} */}
     </div>
   );
 }
